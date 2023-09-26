f296900b02ef2feffea8617ac3463cddd18f3d79
leticiadocrime <106694092+leticiadocrime@users.noreply.github.com>
2023-09-26 13:23:14 -0300

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,5 @@
+// creating an array and passing the number, questions, options and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes 
